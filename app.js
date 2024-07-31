ebf94e3ef738ec7f267515436a000abcfb2fa115
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-01 00:39:38 +0530

    created the express app in which created a http server which also associated with the socket io server and also created a chess instance to get the data and the rules of the chess from the chessJs

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const express = require('express')
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const { Chess } = require('chess.js')
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
