d3c3b55eba7ef5971683e27de1b6e09ca5367bc6
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-04 01:19:34 +0530

    creating the first route for the index page created a new ejs file.. to start the app used server.listen instead app and rendered the index page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
 const { Chess } = require('chess.js')
+const path = require("path")
 
 const app = express();
 
@@ -17,3 +18,11 @@ let CurrentPlayer = "W";
 
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+server.listen(3000, function() {
+    console.log("listening on port 3000")
+});
