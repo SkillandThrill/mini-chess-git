014bd21f677bdb05c5852cf72f7bd40e2bc3dca5
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-08-04 00:12:23 +0530

    adding variables to the codebase as the first user 'W' and the Currentplayer then to use the ejs using the app.set as view engine and other parameter to be ejs. adding to that including the stsatic files.

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ const server = http.createServer(app);
 const io = socket(server);
 
 const chess = new Chess();
+
+
+let players = {};
+let CurrentPlayer = "W";
+
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
